5cade3847f7f703e0ea9a19be2875dc9f228f571
Gabriele31201 <gabri31201@gmail.com>
2023-05-30 12:42:02 +0200

    add for-in

---
+++ js/main.js
@@ -0,0 +1,28 @@
+ 
+ //CREARE L' OGGETTO STUDENT E METTERE DENTRO NOME, COGNOME, ETA E PESO
+ 
+ const student = {
+    Name: `Gabriele`,
+    Surname: `Simone`,
+    Age: 21,
+    Weight: 59
+
+ };
+
+   for (const key in student) {
+      console.log(student[key]);
+         
+      
+   }
+
+
+
+
+
+
+
+ /* console.log("Nome:" + student.Name)
+ console.log("Cognome" + student.Surname)
+ console.log("Eta" + student.Age)
+ console.log("Peso" + student.Weight)
+ */
