f759d4a3e185eef29d04d81c6442f9b667612a8a
Gabriele31201 <gabri31201@gmail.com>
2023-05-30 12:33:13 +0200

    addd objetct student

---
+++ main.js
@@ -0,0 +1,10 @@
+ 
+ //CREARE L' OGGETTO STUDENT E METTERE DENTRO NOME, COGNOME, ETA E PESO
+ 
+ const student = {
+    Name: `Gabriele`,
+    Surname: `Simone`,
+    Age: 21,
+    Weight: 59
+
+ };
